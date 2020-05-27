002809ce7c41ff6ffc2d95d01d97086f0d3acf2e
yanbo <593989315@qq.com>
2020-05-27 13:38:40 +0800

    封闭查找元素

--- adbCommand.py
+++ adbCommand.py
@@ -9,7 +9,7 @@ def TwoDevices():#解决有两个模拟器问题（offine devices）
     devices=out
     return devices
 def getFocusedPackageAndActivity():
-    pattern = re.compile( r"(.+?)/[a-zA-Z0-9\.]+")  # 这里使用了正则表达式，对输出的内容做了限制，只会显示类似"com.mediatek.factorymode/com.mediatek.factorymode.FactoryMode"的字符串
+    pattern = re.compile(r"(.+?)/.[a-zA-Z0-9\.]+")  # 这里使用了正则表达式，对输出的内容做了限制，只会显示类似"com.mediatek.factorymode/com.mediatek.factorymode.FactoryMode"的字符串
     out = os.popen("adb shell dumpsys window windows | findstr \/ | findstr name=").read()  # window下使用findstr
     list = pattern.findall(out)
     component = list[0]  # 输出列表中的第一条字符串
