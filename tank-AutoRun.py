a7577637b55e424be014f7de82ea959fea8ba3cc
boo-yan <965972450@qq.com>
2020-06-20 08:19:02 +0800

    滑动代码封装

--- tank-AutoRun.py
+++ tank-AutoRun.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This sample code uses the Appium python client
 # pip install Appium-Python-Client
 # Then you can paste this into a file and simply run with Python
@@ -7,26 +8,82 @@ from time import sleep
 import time
 import os
 from selenium.webdriver.support.wait import WebDriverWait
+
 desired_caps = {
-    'platformName':"Android",
-    'deviceName':"emulator-5554",
-    'appPackage':"com.lihao.app",
+    'platformName': "Android",
+    'deviceName': "emulator-5554",
+    'appPackage': "com.huyang.test",
     'appActivity': ".MainActivity",
-    #'app':"D:\\aPackage\\app-debug.apk",
-    'unicodeKeyboard':True,  #使用unicodeKeyboard,即Appiuum自带键盘
-    'resetKeyboard':True,# 重新设置系统键盘为Appium自带键盘pip
-    'noReset':True #每次启动不重置APP,即不执行清空APP数据操作
-   # 'udid':"emulator-5554"
+    # 'app':"D:\\aPackage\\app-debug.apk",
+    'unicodeKeyboard': True,  # 使用unicodeKeyboard,即Appiuum自带键盘
+    'resetKeyboard': True,  # 重新设置系统键盘为Appium自带键盘pip
+    # 'noReset': True  # 每次启动不重置APP,即不执行清空APP数据操作
+    # 'udid':"emulator-5554"
 
 }
-driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_caps)
-#driver.implicitly_wait(5) #隐式等待，5s里找到元素就开始执行 time.sleep(5)必须等待5s才开始执行
-#WebDriverWait(driver,6).until(lambda x:x.find_element_by_id('com.mymoney:id/next_btn'))
-#向左滑动4次，跳转引导页,调用swip方法，需要添加time.sleep(s)等待时间，否则会报错
+driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+# driver.implicitly_wait(5) #隐式等待，5s里找到元素就开始执行 time.sleep(5)必须等待5s才开始执行
+# WebDriverWait(driver,6).until(lambda x:x.find_element_by_id('com.mymoney:id/next_btn'))
+# 向左滑动4次，跳转引导页,调用swip方法，需要添加time.sleep(s)等待时间，否则会报错
 time.sleep(1)
+
+
+def touch_tap(self, x, y, duration=100):  # 点击坐标  ,x1,x2,y1,y2,duration
+    '''
+    method explain:点击坐标
+    parameter explain：【x,y】坐标值,【duration】:给的值决定了点击的速度
+    Usage:
+        device.touch_coordinate(277,431)      #277.431为点击某个元素的x与y值
+    '''
+    screen_width = self.driver.get_window_size()['width']  # 获取当前屏幕的宽
+    screen_height = self.driver.get_window_size()['height']  # 获取当前屏幕的高
+    a = (float(x) / screen_width) * screen_width
+    x1 = int(a)
+    b = (float(y) / screen_height) * screen_height
+    y1 = int(b)
+    self.driver.tap([(x1, y1), (x1, y1)], duration)
+
+
+def get_size():
+    x = driver.get_window_size()['width']
+    y = driver.get_window_size()['height']
+    return x, y
+
+
+def swipLeft():
+    l = get_size()
+    x1 = int(l[0] * 0.9)
+    x2 = int(l[1] * 0.2)
+    y1 = int(l[0] * 0.5)
+    driver.swipe(x1, y1, x2, y1, 1000)
+
+
+def swipRight():
+    R = get_size()
+    x1 = int(R[0] * 0.2)
+    x2 = int(R[1] * 0.9)
+    y1 = int(R[0] * 0.5)
+    driver.swipe(x1, y1, x2, y1, 1000)
+
+
+def swipUp():
+    U = get_size()
+    y1 = int(U[0]*0.8)
+    y2 = int(U[1]*0.2)
+    x1 = int(U[0]*0.2)
+    driver.swipe(x1,y1,x1,y2,1000)
+
+def swipDown():
+    U = get_size()
+    y1 = int(U[0]*0.2)
+    y2 = int(U[1]*0.8)
+    x1 = int(U[0]*0.2)
+    driver.swipe(x1,y1,x1,y2,1000)
+# 滑动app起始页，4页
 for i in range(4):
-    getSize.swipLeft()#调用左滑方法
+    # 调用左滑方法
+    swipLeft()
     sleep(0.5)
-#点击“立即开始”
+#点击“立即开始”按钮
 TouchAction(driver).tap(x=528, y=1616).perform()
 my=driver.find_element_by_xpath()
--- test.py
+++ test.py
@@ -1,2 +1,55 @@
-import requests
-response=requests.get('http://api.github.com')
+classmate = ['bo', 'li', 'zhang']
+# 列出所有名字
+classmate
+# 获取list元素的个数
+len(classmate)
+# 用索引来访问list中每个元素
+# 访问‘bo’元素
+classmate[0]
+# 访问最后一个元素
+classmate[-1]
+# list是一个有序表
+# 增加元素到末尾
+classmate.append('hong')
+# 增加元素到指定位置
+classmate.insert(1,'dong')
+print(classmate)
+# 删除list末尾的元素,pop(i) i为元素的位置
+classmate.pop(1)
+# 替换某个元素的值
+classmate[0]='gong'
+# list里可以是其它list
+classmate=['2','4',['li','si']]
+
+# tuple 有序列表，初始化后，不能修改指定元素的对应位置
+# tuple中t值改变
+t=('a','b',['c','d'])
+t[2][0]='C'
+t[2][1]='D'
+
+colors=('green','yellow','red')
+# 定义只有一个元素时
+color=('red',)
+# if
+age=20
+if age>10:
+    print('青少年')
+elif age>18:
+    print('成年人')
+# if简写
+x=0
+if x:
+    print('简写的if')
+# input() 读取用户的输入
+'''date=input('date')
+# input返回类型str转换为int
+date=int(date)
+if date>19900101:
+    print("90后")'''
+# range()生成一个整数序列,list()函数转换为list
+list(range(10))
+# 求1-10的和
+sum=0
+for x in range(10):
+    sum=sum+x
+print(sum)
